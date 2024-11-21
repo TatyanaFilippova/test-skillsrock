31b9dcecab960df27a90a05bde5732bb752a1f9c
Tatyana <tatiana.filippova.98@mail.ru>
2024-11-21 11:18:52 +0300

    Глубокое клонирование объекта

--- task-5/index-2.js
+++ task-5/index-2.js
@@ -1,3 +1,35 @@
 function deepClone(obj) {
-  // Ваш код здесь
+  //превращаем объект в массив ключей
+  const keys = Object.keys(obj);
+  //создаем объект для клонированных свойств
+  const result = {};
+  //перебираем все ключи
+  for (const key of keys) {
+    const value = obj[key];
+    //проверяем тип и если "объект", то делаем его глубокое клонирование, иначе просто возвращаем
+    switch (typeof value) {
+      case "object": {
+        result[key] = deepClone(value);
+        break;
+      }
+      default: {
+        result[key] = value;
+        break;
+      }
+    }
+  }
+  return result;
 }
+
+const original = {
+  name: "John",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
+const copy = deepClone(original);
+copy.address.city = "Los Angeles";
+console.log(original.address.city);
+console.log(copy.address.city);
+// Los Angeles
